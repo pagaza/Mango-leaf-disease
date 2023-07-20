e2530070fa782d795567f2634c8263528d7fa2ab
pagaza <sophieplouin@icloud.com>
2023-07-20 15:02:00 -0400

    initial commit

---
+++ mangoLeafDisease.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import jetson_inference
+import jetson_utils
+from jetson_inference import imageNet
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", type=str, help="filename of the image to process")
+opt = parser.parse_args()
+
+img = jetson_utils.loadImage(opt.filename)
+
+net = imageNet(model="models/resnet18.onnx", labels="models/labels.txt", 
+                 input_blob="input_0", output_blob="output_0")
+
+class_idx, confidence = net.Classify(img)
+
+class_desc = net.GetClassDesc(class_idx)
+
+#return image w class and confidence overlay as output
+
+# Code works as expected, but another model should be created using more epochs
+
+print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))  #delete after testing
