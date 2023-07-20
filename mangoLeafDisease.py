ab8489593cf0f280edfc85cbadf3eb1358618b9e
pagaza <sophieplouin@icloud.com>
2023-07-20 15:50:07 -0400

    update

--- mangoLeafDisease.py
+++ mangoLeafDisease.py
@@ -21,6 +21,4 @@ class_desc = net.GetClassDesc(class_idx)
 
 #return image w class and confidence overlay as output
 
-# Code works as expected, but another model should be created using more epochs
-
-print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))  #delete after testing
+print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))  
